c0fcda42289b75ef4241a88f3721a6777c10bcc9
not-felipe <felipeam@id.uff.br>
2023-02-07 10:16:02 -0300

    commit

---
+++ matrizes - 07-02-2023/matriz.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+  int l, c, *M, n = 4;
+
+  printf("Digite o numero de linhas e de colunas: ");
+  scanf("%d %d", &l, &c);
+
+  M = (int*)malloc(l*c*sizeof(int)); // Aloca a matriz com l linhas e c colunas
+
+  for (int i = 0; i < l; i++){
+    for (int j = 0; j < c; j++){
+      int k = i * n + j;
+      M[k] = k;
+    }
+  }
+
+  for (int i = 0; i < l; i++){
+    for (int j = 0; j < c; j++){
+      int k = i * n + j;
+      printf("%d ", M[k]);
+    }
+    printf("\n");
+  }
+
+  free(M);
+  return 0;
+}
